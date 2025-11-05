526044e994bb57b5643b4aeb9a0070ad3fa7a8a6
nekruzrakhimov <nekruzusa111@gmail.com>
2025-11-06 00:07:02 +0500

    chore:  updates

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@ func Load() *Config {
 	return &Config{
 		Port:              getEnv("PORT", ":8283"),
 		AuthServiceURL:    getEnv("AUTH_SERVICE_URL", "http://localhost:8284"),
-		ProductServiceURL: getEnv("PRODUCT_SERVICE_URL", "http://localhost:7780"),
+		ProductServiceURL: getEnv("PRODUCT_SERVICE_URL", "http://localhost:8285"),
 	}
 }
 
